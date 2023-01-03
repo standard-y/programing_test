3d770ed97cf980f26a07f72a92d117f2dd693243
jun <standard0506@gmail.com>
2023-01-03 10:42:43 +0900

    바둑알 십자 뒤집기

---
+++ codeup_6096/바둑알 십자 뒤집기.py
@@ -0,0 +1,19 @@
+
+# [1][1] 
+# 
+# [1][0]
+# [1][-1]
+lis = []
+
+
+for i in range(19):
+    lis.append([])
+    for j in range(19):
+        lis[i].append(0)
+
+for i in range(19):
+    go = list(int, map(input().split()))
+    for j in range(19):
+        lis[j][i]
+
+lis = int(not bool(go))
